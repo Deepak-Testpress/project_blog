325d949ba1ea99836b30fd7491474d681c0a1f4a
Deepak-Testpress <deepak@testpress.in>
2022-12-06 11:18:46 +0530

    Handle CommentForm in detail view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, FormView
 from django.core.mail import send_mail
-from blog.forms import EmailPostForm
+from blog.forms import EmailPostForm, CommentForm
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 
@@ -23,7 +23,29 @@ def post_detail(request, year, month, day, post_slug):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+        },
+    )
 
 
 class PostShareView(SuccessMessageMixin, FormView):
